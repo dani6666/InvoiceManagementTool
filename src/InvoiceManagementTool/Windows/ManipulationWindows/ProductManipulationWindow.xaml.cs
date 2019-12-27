568d417025b22d7cdf16cda3ab55c86eefe08634
Bialy6000 <danielwz99@gmail.com>
2019-12-27 14:10:58 +0100

    minor changes in windows

--- src/InvoiceManagementTool/Windows/ManipulationWindows/ProductManipulationWindow.xaml.cs
+++ src/InvoiceManagementTool/Windows/ManipulationWindows/ProductManipulationWindow.xaml.cs
@@ -18,5 +18,22 @@ namespace InvoiceManagementTool.Windows.ManipulationWindows
         {
             throw new NotImplementedException();
         }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void UpdateButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(int.TryParse(StorageAmountTextBox.Text, out int amount) && float.TryParse(PriceTextBox.Text, out float price))
+            {
+
+            }
+            else
+            {
+                MessageBox.Show("Invalid input");
+            }
+        }
     }
 }
